ee73ee4ce86f0d46df70fa624dbed17ad4def55c
raamadithyan <chitha.don@gmail.com>
2023-08-03 00:28:12 +0530

    production build check_1

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import {Roboto} from 'next/font/google';
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="z-10 absolute top-[50%] left-[50%] translate-x-[-50%]">
+    <div>
+    Production build check_1
+    </div> 
     
     </main> 
   )
 }
-
