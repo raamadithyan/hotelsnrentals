fc9aba8ef1b174b09a344599fecb7895d411c3ec
raamadithyan <chitha.don@gmail.com>
2023-08-03 13:14:51 +0530

    categories-ui closed

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -5,11 +5,19 @@ import Logo from "./Logo";
 import Search from "./Search";
 import Login from "./Login";
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import Categories from "./Categories";
 function Navbar() {
   return (
     <div className="fixed w-full  z-0 bg-white  ">
       <div className="py-4 border-b-[1px]">
+=======
+import Categories from './Categories'
+function Navbar() {
+  return (
+    <div className="fixed w-full  z-0  bg-white shadow-sm border-[1px]   ">
+      <div className="py-4 border-b-[1px] shadow-sm ">
+>>>>>>> Stashed changes
 =======
 import Categories from './Categories'
 function Navbar() {
