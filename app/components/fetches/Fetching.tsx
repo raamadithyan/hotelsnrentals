e885196369129519ea747ef967725f7b4b791e93
raamadithyan <chitha.don@gmail.com>
2023-08-11 16:48:17 +0530

    RSC fetch draft success

--- app/components/fetches/Fetching.tsx
+++ app/components/fetches/Fetching.tsx
@@ -3,16 +3,43 @@ interface FetchProps {
 
 }
 
+const url =
+  'https://airbnb13.p.rapidapi.com/search-location?location=Delhi&checkin=2023-09-16&checkout=2023-09-17&adults=1&children=0&infants=0&pets=0&page=4&currency=USD';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'd86894edc4msh629ee0c98e8c983p1fda9bjsn154dcafda454',
+    'X-RapidAPI-Host': 'airbnb13.p.rapidapi.com',
+  },
+};
+
 
 const Fetching:React.FC<FetchProps> = async  ({isOpen}) => {
 
-	const data = await fetch("")
+// async function Fetching({isOpen}:{isOpen:boolean;}){
+
+	const res = await fetch(url,options)
+	      const data = await res.json();
+	            const result = data.results;
+	            // console.log(result)
+
+
+
 	return (
-		<div>
-
-		Fetching data ...
-
+		<>
+		<div className="flex flex-wrap">
+			{result && result.map((res)=>(
+				<div className=""  key={res.id}>
+				<img width="500" src={res.images[0]}/>
+					
+				</div>
+				))}
+				
 		</div>
+
+		
+
+		</>
 	)
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
+
 import {Roboto} from 'next/font/google';
  import useFetchState from './hooks/useFetchState';
  import Fetching from './components/fetches/Fetching';
 
 export default function Home() {
 
-const fetch =  useFetchState()
-
+// const fetch =  useFetchState()
   return (
-    <main className="-z-10 absolute top-[30%]  border-2 border-red-500 w-[100vw] ">
-    <div className='m-16 border-2 border-red-500'>
-    <Fetching isOpen={fetch.isFetch}  />
+    <main className="-z-10 absolute  top-[30%]  border-2 border-red-500 w-[100vw] ">
+    <div className=' border-2 border-red-500'>
+    <Fetching isOpen={useFetchState.getState().isFetch} />
     
    
     
